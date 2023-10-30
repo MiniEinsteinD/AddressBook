eaa50b5dec769b17a8ee8b0f34bad244c4800e44
Daniah Mohammed <daniah.mohamad1999@gmail.com>
2023-10-30 14:24:03 -0400

    creating more https

--- src/main/java/com/example/lab03/AddressBookController.java
+++ src/main/java/com/example/lab03/AddressBookController.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AddressBookController {
-
     @Autowired
     AddressBookRepository addressBookRepository;
 
@@ -18,6 +18,33 @@ public class AddressBookController {
         AddressBook addressBook = addressBookRepository.findByOwner(owner);
         model.addAttribute("addressBook", addressBook);
         return "addressBook";
+    }
+    @GetMapping("/createAddressBook")
+    public String createAddressBookForm() {
+        return "createAddressBook";
+    }
+    @PostMapping("/createAddressBook")
+    public String createAddressBook(@RequestParam String owner) {
+        AddressBook addressBook = new AddressBook(owner);
+        addressBookRepository.save(addressBook);
+        return "redirect:/addressbook?owner=" + owner;
+    }
+    @GetMapping("/addBuddy")
+    public String addBuddyForm() {
+        return "addBuddy";
+    }
+
+    @PostMapping("/addBuddy")
+    public String addBuddy(@RequestParam Long id, @RequestParam String name, @RequestParam String phoneNumber) {
+        // Retrieve the address book you want to add the buddy to (you can modify this logic)
+        AddressBook addressBook = addressBookRepository.findByOwner("Daniah");
+
+        if (addressBook != null) {
+            BuddyInfo buddy = new BuddyInfo(id, name, phoneNumber);
+            addressBook.addBuddy(buddy);
+            addressBookRepository.save(addressBook);
+        }
+        return "redirect:/addressbook?owner=Daniah";
     }
 
 
--- src/main/java/com/example/lab03/BuddyInfoRepository.java
+++ src/main/java/com/example/lab03/BuddyInfoRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface BuddyInfoRepository extends CrudRepository<BuddyInfo, Long> {
-
     Optional<BuddyInfo> findById(Long id);
     Iterable<BuddyInfo> findAll();
 }
